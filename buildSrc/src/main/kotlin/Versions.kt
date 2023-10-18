d0db05b335b7ac889e3610cbcbdb42ddb9d17fdf
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-18 11:28:23 +0200

    updating versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ package buildSrc.src.main.kotlin
 
 object Versions {
     //----- COMPOSE ----------/
-    const val composeVersion = "2022.10.00"
+    const val composeVersion = "2023.10.00"
     const val activityComposeVersion = "1.8.0"
     const val navigationComposeVersion = "2.7.4"
 }
