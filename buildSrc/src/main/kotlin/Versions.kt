0b1837f8e3b79dacf7c780b69a156877ac7aa74e
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-10-25 19:11:13 +0200

    updating gradle versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,13 @@
 package buildSrc.src.main.kotlin
 
 object Versions {
+    //----- ANDROIDX ----------/
+    const val appCompatVersion = "1.6.1"
     //----- COMPOSE ----------/
-    const val composeVersion = "2023.10.00"
     const val activityComposeVersion = "1.8.0"
+    const val composeVersion = "2023.10.00"
     const val navigationComposeVersion = "2.7.4"
+    //----- GOOGLE ----------/
+    const val hiltVersion = "2.44.2"
+    const val materialVersion = "1.10.0"
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -3,6 +3,8 @@ import buildSrc.src.main.kotlin.Versions
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -24,14 +26,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -41,10 +44,15 @@ android {
 dependencies {
 
     //----- AAR ----------/
-    implementation(files("libs/mzs-presentation.aar"))
+    implementation(files(project.rootDir.resolve("libs/mzs-domain.aar")))
+    implementation(files(project.rootDir.resolve("libs/mzs-presentation.aar")))
 
-    //----- SUBMODULES ----------/
-    implementation(project(":mzs-presentation"))
+    //----- MODULES ----------/
+    implementation(project(":domain"))
+
+    //----- ANDROIDX ----------/
+    implementation("androidx.appcompat:appcompat:${Versions.appCompatVersion}")
+    implementation("androidx.core:core-ktx:1.12.0")
 
     //----- COMPOSE ----------/
     implementation("androidx.compose.ui:ui")
@@ -55,4 +63,13 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:${Versions.composeVersion}"))
     debugImplementation("androidx.compose.ui:ui-tooling")
 
+    //----- GOOGLE ----------/
+    implementation("com.google.android.material:material:${Versions.materialVersion}")
+    implementation("com.google.dagger:hilt-android:${Versions.hiltVersion}")
+    kapt("com.google.dagger:hilt-android-compiler:${Versions.hiltVersion}")
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
