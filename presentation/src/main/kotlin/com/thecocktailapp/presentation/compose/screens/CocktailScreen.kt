2ceaaca5692a225f8c7d9553180911a418d19e5a
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-11-13 13:21:10 +0100

    adding cocktailscreen

---
+++ presentation/src/main/kotlin/com/thecocktailapp/presentation/compose/screens/CocktailScreen.kt
@@ -0,0 +1,12 @@
+package com.thecocktailapp.presentation.compose.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CocktailScreen(
+    modifier: Modifier = Modifier,
+) {
+
+
+}
