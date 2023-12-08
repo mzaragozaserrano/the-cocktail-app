6dc88f6c3c678c0b0230747771c297e594c1088d
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2023-12-08 17:17:08 +0100

    adding resourcesdatasource

--- app/src/test/kotlin/com/thecocktailapp/repositories/FakeCocktailRepositoryImpl.kt
+++ app/src/test/kotlin/com/thecocktailapp/repositories/FakeCocktailRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.thecocktailapp.repositories
 
+import com.mzaragozaserrano.data.datasources.local.ResourcesDataSource
 import com.mzaragozaserrano.data.utils.ResultData
 import com.mzaragozaserrano.domain.utils.Result
 import com.thecocktailapp.data.datasources.services.CocktailDataSource
@@ -11,15 +12,22 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class FakeCocktailRepositoryImpl @Inject constructor(private val cocktailDataSource: CocktailDataSource) :
-    CocktailRepository {
+class FakeCocktailRepositoryImpl @Inject constructor(
+    private val cocktailDataSource: CocktailDataSource,
+    private val resourcesDataSource: ResourcesDataSource,
+) : CocktailRepository {
 
     override suspend fun getDrinkById(id: Int): Flow<Result<CocktailBO>> =
         flow {
             emit(Result.Loading)
             emit(
                 when (val result = cocktailDataSource.getDrinkById(id)) {
-                    is ResultData.Response -> Result.Response.Success(result.data.transform())
+                    is ResultData.Response -> Result.Response.Success(
+                        result.data.transform(
+                            resourcesDataSource
+                        )
+                    )
+
                     is ResultData.Error<*> -> Result.Response.Error((result.code as ErrorDTO).transform())
                 }
             )
@@ -30,10 +38,19 @@ class FakeCocktailRepositoryImpl @Inject constructor(private val cocktailDataSou
             emit(Result.Loading)
             emit(
                 when (val result = cocktailDataSource.getRandomDrink()) {
-                    is ResultData.Response -> Result.Response.Success(result.data.transform())
+                    is ResultData.Response -> Result.Response.Success(
+                        result.data.transform(
+                            resourcesDataSource
+                        )
+                    )
+
                     is ResultData.Error<*> -> Result.Response.Error((result.code as ErrorDTO).transform())
                 }
             )
         }
+
+    override fun showRandomCocktail(): Boolean {
+        TODO("Aquí hay que implementar el test para saber si se va a mostrar o no el splash")
+    }
 
 }
