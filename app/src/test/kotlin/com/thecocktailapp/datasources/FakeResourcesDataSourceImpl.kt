1f5a205897430baef3eada219dead4ff96501a67
mzaragoza <miguel.zaragozaserrano@plexus.es>
2024-12-27 19:31:44 +0100

    refactoring params

--- app/src/test/kotlin/com/thecocktailapp/datasources/FakeResourcesDataSourceImpl.kt
+++ app/src/test/kotlin/com/thecocktailapp/datasources/FakeResourcesDataSourceImpl.kt
@@ -8,6 +8,6 @@ class FakeResourcesDataSourceImpl @Inject constructor() : ResourcesDataSource {
     override fun getStringFromResource(resId: Int): String =
         "Pour Hot Damn 100 in bottom of a jar or regular glass.Fill the rest of the glass with sweet tea.Stir with spoon, straw, or better yet a cinnamon stick and leave it in ."
 
-    override fun getStringOrResource(str: String?, resId: Int): String = str ?: "Coffee / Tea"
+    override fun getStringOrResource(resId: Int, str: String?, ): String = str ?: "Coffee / Tea"
 
 }
