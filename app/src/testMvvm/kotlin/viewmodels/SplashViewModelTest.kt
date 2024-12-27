01cfa8784c155568ebbef0e8393b0e3a02058ccf
mzaragoza <miguel.zaragozaserrano@plexus.es>
2024-12-27 19:31:59 +0100

    fixing test

--- app/src/testMvvm/kotlin/viewmodels/SplashViewModelTest.kt
+++ app/src/testMvvm/kotlin/viewmodels/SplashViewModelTest.kt
@@ -8,6 +8,8 @@ import com.thecocktailapp.presentation.viewmodels.SplashViewModel
 import com.thecocktailapp.presentation.vo.DrinkType
 import com.thecocktailapp.presentation.vo.DrinkVO
 import com.thecocktailapp.presentation.vo.ErrorVO
+import com.thecocktailapp.presentation.vo.SplashSuccess
+import com.thecocktailapp.presentation.vo.SplashUiState
 import com.thecocktailapp.repositories.FakeCocktailRepositoryImpl
 import com.thecocktailapp.repositories.FakeNetworkRepositoryImpl
 import com.thecocktailapp.usecases.FakeGetRandomDrinkUseCaseImpl
@@ -115,23 +117,25 @@ class SplashViewModelTest {
     @Test
     fun `when getRandomDrinkUseCase is invoked, loading result is emitted first, followed by a success result`() =
         runTest {
-            viewModel.state.test {
-                assertEquals(SplashViewModel.SplashUiState.Loading, awaitItem())
+            viewModel.uiState.test {
+                assertEquals(SplashUiState(loading = true), awaitItem())
                 assertEquals(
-                    SplashViewModel.SplashUiState.Success(
-                        DrinkVO(
-                            category = "Coffee / Tea",
-                            dateModified = "2015-09-03 03:09:44",
-                            drinkType = DrinkType.Alcoholic,
-                            glass = "Mason jar",
-                            id = "15813",
-                            name = "Herbal flame",
-                            ingredients = listOf(
-                                "Hot Damn - 5 shots",
-                                "Tea - very sweet"
-                            ),
-                            instructions = "Pour Hot Damn 100 in bottom of a jar or regular glass.Fill the rest of the glass with sweet tea.Stir with spoon, straw, or better yet a cinnamon stick and leave it in .",
-                            urlImage = "https://www.thecocktaildb.com/images/media/drink/rrstxv1441246184.jpg"
+                    SplashUiState(
+                        success = SplashSuccess(
+                            DrinkVO(
+                                category = "Coffee / Tea",
+                                dateModified = "2015-09-03 03:09:44",
+                                drinkType = DrinkType.Alcoholic,
+                                glass = "Mason jar",
+                                id = "15813",
+                                name = "Herbal flame",
+                                ingredients = listOf(
+                                    "Hot Damn - 5 shots",
+                                    "Tea - very sweet"
+                                ),
+                                instructions = "Pour Hot Damn 100 in bottom of a jar or regular glass.Fill the rest of the glass with sweet tea.Stir with spoon, straw, or better yet a cinnamon stick and leave it in .",
+                                urlImage = "https://www.thecocktaildb.com/images/media/drink/rrstxv1441246184.jpg"
+                            )
                         )
                     ),
                     awaitItem()
@@ -142,9 +146,9 @@ class SplashViewModelTest {
     @Test
     fun `when getRandomDrinkUseCase is not invoked, loading result is emitted first, followed by a data not found error result`() =
         runTest {
-            viewModel.state.test {
+            viewModel.uiState.test {
                 assertEquals(
-                    SplashViewModel.SplashUiState.Error(ErrorVO.DataNotFound),
+                    SplashUiState(error = ErrorVO.DataNotFound),
                     awaitItem()
                 )
             }
@@ -153,9 +157,9 @@ class SplashViewModelTest {
     @Test
     fun `when getRandomDrinkUseCase is invoked but a connectivity error result is emitted first`() =
         runTest {
-            viewModel.state.test {
+            viewModel.uiState.test {
                 assertEquals(
-                    SplashViewModel.SplashUiState.Error(ErrorVO.Connectivity),
+                    SplashUiState(error = ErrorVO.Connectivity),
                     awaitItem()
                 )
             }
